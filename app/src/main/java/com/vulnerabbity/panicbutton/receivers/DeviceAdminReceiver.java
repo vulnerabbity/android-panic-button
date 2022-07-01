62383114859629ebc86dccd88df1faadca654768
vulnerabbity <none@example.com>
2022-07-01 19:44:07 +0700

    new class: (DeviceAdminReceiver)

---
+++ app/src/main/java/com/vulnerabbity/panicbutton/receivers/DeviceAdminReceiver.java
@@ -0,0 +1,6 @@
+package com.vulnerabbity.panicbutton.receivers;
+
+import com.vulnerabbity.panicbutton.utils.DefaultBroadcastReceiver;
+
+public class DeviceAdminReceiver extends DefaultBroadcastReceiver {
+}
