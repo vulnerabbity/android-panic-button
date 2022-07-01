4f294019fa07ca58d0de2615a733ddf825c336f8
vulnerabbity <none@example.com>
2022-07-01 20:55:17 +0700

    add listening to notification close

--- app/src/main/java/com/vulnerabbity/panicbutton/notifications/AppNotificationsManager.java
+++ app/src/main/java/com/vulnerabbity/panicbutton/notifications/AppNotificationsManager.java
@@ -4,6 +4,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 
+import com.vulnerabbity.panicbutton.receivers.PanicActionReceiver;
+
 public class AppNotificationsManager {
   private Context context;
   private AppNotificationsBuilder notificationsBuilder;
@@ -19,15 +21,18 @@ public class AppNotificationsManager {
     display(notification, config.id);
   }
 
-  public void displayClickingListeningNotification(NotificationConfig config) {
+  public void displayLockListeningNotification(NotificationConfig config) {
     Notification notification = notificationsBuilder.buildNotification(config);
 
-    addClickListeningOnNotification(notification);
+    addLockListeningToNotification(notification);
     display(notification, config.id);
   }
 
-  private void addClickListeningOnNotification(Notification notification) {
-    notification.contentIntent = NotificationClickedActionReceiver.getPendingIntent(this.context);
+  private void addLockListeningToNotification(Notification notification) {
+    // on click
+    notification.contentIntent = PanicActionReceiver.getPendingIntent(this.context);
+    // on close swipe
+    notification.deleteIntent = PanicActionReceiver.getPendingIntent(this.context);
   }
 
   private void display(Notification notification, int id) {
