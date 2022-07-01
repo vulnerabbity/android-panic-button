f713e2fc81f82e9bd9e17f05d02cbdd85d9372f6
vulnerabbity <none@example.com>
2022-07-01 17:14:02 +0700

    new: add notification click listener

--- app/src/main/java/com/vulnerabbity/panicbutton/MainActivity.java
+++ app/src/main/java/com/vulnerabbity/panicbutton/MainActivity.java
@@ -3,11 +3,15 @@ package com.vulnerabbity.panicbutton;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.vulnerabbity.panicbutton.notifications.NotificationClickedActionReceiver;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    NotificationClickedActionReceiver.register(this);
   }
 }
