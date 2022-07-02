4ee7f5e6b248bdd5e839ed016a6e7f9ea0ca7b59
vulnerabbity <none@example.com>
2022-07-02 20:04:11 +0700

    add separate panic actions

--- app/src/main/java/com/vulnerabbity/panicbutton/events/ApplicationEvents.java
+++ app/src/main/java/com/vulnerabbity/panicbutton/events/ApplicationEvents.java
@@ -3,7 +3,10 @@ package com.vulnerabbity.panicbutton.events;
 import com.vulnerabbity.panicbutton.libs.observer.Subject;
 
 public class ApplicationEvents {
-  public static Subject<String> panicActionCalled$ = new Subject();
+  public static Subject<String> panicAction1Called$ = new Subject();
+  public static Subject<String> panicAction2Called$ = new Subject();
+  public static Subject<String> panicNotificationClosed$ = new Subject();
+
   public static Subject<String> panicModeEnabled$ = new Subject();
   public static Subject<String> panicModeDisabled$ = new Subject();
 }
--- app/src/main/java/com/vulnerabbity/panicbutton/receivers/PanicNotificationActionReceiver.java
+++ app/src/main/java/com/vulnerabbity/panicbutton/receivers/PanicNotificationActionReceiver.java
@@ -5,41 +5,68 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.widget.RemoteViews;
 
+import com.vulnerabbity.panicbutton.R;
 import com.vulnerabbity.panicbutton.events.ApplicationEvents;
-import com.vulnerabbity.panicbutton.utils.logger.Logger;
 
-class PanicActions {
-  
+class PANIC_ACTIONS {
+  static final String CLOSE = "PANIC_NOTIFICATION_CLOSE";
+
+  static final String PANIC_ACTION_1 = "PANIC_NOTIFICATION_ACTION_1";
+
+  static final String PANIC_ACTION_2 = "PANIC_NOTIFICATION_ACTION_2";
 }
 
-public class PanicActionReceiver extends BroadcastReceiver {
+public class PanicNotificationActionReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
     String action = intent.getAction();
-    if (action == ACTION_NAME) {
-      emitNotificationClicked();
+    if (action == PANIC_ACTIONS.PANIC_ACTION_1) {
+      emitPanicAction1Called();
+    }
+    if (action == PANIC_ACTIONS.PANIC_ACTION_2) {
+      emitPanicAction2Called();
+    }
+    if (action == PANIC_ACTIONS.CLOSE) {
+      emitPanicNotificationClosed();
     }
   }
 
   public static void register(Context context) {
-    PanicActionReceiver receiver = new PanicActionReceiver();
+    PanicNotificationActionReceiver receiver = new PanicNotificationActionReceiver();
     IntentFilter intentFilter = new IntentFilter();
     intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
-    intentFilter.addAction(ACTION_NAME);
+
+    intentFilter.addAction(PANIC_ACTIONS.PANIC_ACTION_1);
+    intentFilter.addAction(PANIC_ACTIONS.PANIC_ACTION_2);
+    intentFilter.addAction(PANIC_ACTIONS.CLOSE);
 
     context.registerReceiver(receiver, intentFilter);
   }
 
-  public static PendingIntent getPendingIntent(Context context) {
-    Intent intent = new Intent(ACTION_NAME);
+  public static void addListeningToNotification(Context context, RemoteViews panicNotification) {
+    panicNotification.setOnClickPendingIntent(R.id.panic_notification_action_1, makePendingIntent(context, PANIC_ACTIONS.PANIC_ACTION_1));
+    panicNotification.setOnClickPendingIntent(R.id.panic_notification_action_2, makePendingIntent(context, PANIC_ACTIONS.PANIC_ACTION_2));
+    panicNotification.setOnClickPendingIntent(R.id.panic_notification_close, makePendingIntent(context, PANIC_ACTIONS.CLOSE));
+  }
+
+  private static PendingIntent makePendingIntent(Context context, String actionName) {
+    Intent intent = new Intent(actionName);
     PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
     return pendingIntent;
   }
 
-  private void emitNotificationClicked() {
-    Logger.log(ACTION_NAME);
-    ApplicationEvents.panicActionCalled$.next("notificationClicked");
+  private void emitPanicAction1Called() {
+    ApplicationEvents.panicAction1Called$.next("panic 1 called");
+  }
+
+  private void emitPanicAction2Called() {
+    ApplicationEvents.panicAction2Called$.next("panic 2 called");
+  }
+
+  private void emitPanicNotificationClosed() {
+    ApplicationEvents.panicNotificationClosed$.next("notification closed");
   }
 }
