85871dbf880a8de632f8a90a1b76704bdca302d1
vulnerabbity <none@example.com>
2022-07-02 19:55:54 +0700

    changed notifications manager api

--- app/src/main/java/com/vulnerabbity/panicbutton/services/PanicForegroundService.java
+++ app/src/main/java/com/vulnerabbity/panicbutton/services/PanicForegroundService.java
@@ -9,14 +9,13 @@ import com.vulnerabbity.panicbutton.events.ApplicationEvents;
 import com.vulnerabbity.panicbutton.libs.panic.PanicListener;
 import com.vulnerabbity.panicbutton.libs.storage.registry.StorageRegistry;
 import com.vulnerabbity.panicbutton.notifications.AppNotificationsManager;
-import com.vulnerabbity.panicbutton.notifications.NotificationConfig;
-import com.vulnerabbity.panicbutton.receivers.PanicActionReceiver;
+import com.vulnerabbity.panicbutton.receivers.PanicNotificationActionReceiver;
 import com.vulnerabbity.panicbutton.utils.logger.Logger;
 
 public class PanicForegroundService extends Service {
   private AppNotificationsManager notificationsManager;
   private PanicListener panicListener;
-  private final int NOTIFICATION_ID = 12345;
+  private final int PANIC_NOTIFICATION_ID = 12345;
   private StorageRegistry storage;
 
   @Nullable
@@ -38,8 +37,8 @@ public class PanicForegroundService extends Service {
     panicListener = new PanicListener(this);
     storage = new StorageRegistry(this);
 
-    // listen to clicks on notification, etc
-    PanicActionReceiver.register(this);
+    // listen to clicks on notification
+    PanicNotificationActionReceiver.register(this);
 
     handleNotificationsEvents();
     initNotification();
@@ -64,17 +63,11 @@ public class PanicForegroundService extends Service {
     return this.storage.isPanicEnabled.get();
   }
 
-  // todo: get title and tex from user
   private void displayNotification() {
-    NotificationConfig config = new NotificationConfig();
-    config.isPublic = true;
-    config.title = "Facebook";
-    config.text = "John sent you message. Swipe to view";
-    config.id = NOTIFICATION_ID;
-    this.notificationsManager.displayLockListeningNotification(config);
+    this.notificationsManager.displayPanicNotification(PANIC_NOTIFICATION_ID);
   }
 
   private void hideNotification() {
-    this.notificationsManager.destroyNotification(NOTIFICATION_ID);
+    this.notificationsManager.destroyNotification(PANIC_NOTIFICATION_ID);
   }
 }
