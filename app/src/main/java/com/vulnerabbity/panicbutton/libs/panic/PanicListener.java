149f6ae971c874dc0089313d09e3b69dffc751db
vulnerabbity <none@example.com>
2022-07-02 17:37:00 +0700

    new: (PanicListener) add handlers for enabled/disabled mode

--- app/src/main/java/com/vulnerabbity/panicbutton/libs/panic/PanicListener.java
+++ app/src/main/java/com/vulnerabbity/panicbutton/libs/panic/PanicListener.java
@@ -3,26 +3,54 @@ package com.vulnerabbity.panicbutton.libs.panic;
 import android.content.Context;
 
 import com.vulnerabbity.panicbutton.events.ApplicationEvents;
-import com.vulnerabbity.panicbutton.libs.observer.Subscription;
+import com.vulnerabbity.panicbutton.libs.storage.registry.StorageRegistry;
 import com.vulnerabbity.panicbutton.utils.logger.Logger;
 
 public class PanicListener {
-  private Subscription<String> panicEventSubscription;
   private PanicExecutor panicExecutor;
+  private StorageRegistry storage;
 
   public PanicListener(Context context) {
-    panicEventSubscription = ApplicationEvents.panicActionCalled$.subscribe((e) -> {
-      this.callPanicAction();
-    });
     panicExecutor = new PanicExecutor(context);
+    storage = new StorageRegistry(context);
+
+    startHandlingEvents();
   }
 
-  public void stopListening() {
-    this.panicEventSubscription.unsubscribe();
+  private void startHandlingEvents() {
+    ApplicationEvents.panicActionCalled$.subscribe((e) -> {
+      callPanicIfPanicMode();
+    });
+
+    ApplicationEvents.panicModeEnabled$.subscribe((e) -> {
+      enablePanicMode();
+    });
+
+    ApplicationEvents.panicModeDisabled$.subscribe((e) -> {
+      disablePanicMode();
+    });
+  }
+
+  private void callPanicIfPanicMode() {
+    if (isPanicMode()) {
+      this.callPanicAction();
+    }
   }
 
   private void callPanicAction() {
     Logger.log("Panic executed");
     this.panicExecutor.lockDevice();
+  }
+
+  private void enablePanicMode() {
+    storage.isPanicEnabled.set(true);
+  }
+
+  private void disablePanicMode() {
+    storage.isPanicEnabled.set(false);
+  }
+
+  private Boolean isPanicMode() {
+    return storage.isPanicEnabled.get();
   }
 }
