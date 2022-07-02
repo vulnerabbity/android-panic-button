16d48b58b58f4e5dcf91f0b4c72a2bf899e326a3
vulnerabbity <none@example.com>
2022-07-02 21:57:17 +0700

    add panic actions

---
+++ app/src/main/java/com/vulnerabbity/panicbutton/libs/panic/PanicActions.java
@@ -0,0 +1,6 @@
+package com.vulnerabbity.panicbutton.libs.panic;
+
+public class PanicActions {
+  public static final String NONE = "PANIC_ACTION_NONE";
+  public static final String LOCK = "PANIC_ACTION_LOCK";
+}
